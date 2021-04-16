5b5c6373d3dc469e3290ae7246ecd81155a8ebc7
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-04-16 13:45:17 +0300

    INITIAL commit

---
+++ HW10-2-client/client.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW10-2-client
+//
+//  Created by Alyaev Roman on 16.04.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
